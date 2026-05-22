d3ceaeb242692da091d33340b6b236150fc798bf
Angelise Perez <angelisep3@nycstudents.net>
2026-05-22 12:36:10 -0400

    attempt#1

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let submit = document.querySelector(".submit");
 
+ let date = document.querySelector("#date").value;
+
 submit.addEventListener("click", function(e) {
-  
-  
+  e.preventDefault();
     // localStorage here
   
-  
+  localStorage.getItem("local storage", "#date")
   
   // sessionStorage here
 
-
+sessionStorage.getItem("riddle", "#date")
 });
